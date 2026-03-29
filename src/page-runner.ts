01ecc7e06d245571bd9066ca4a5a0238b9815d5f
苗雨润 <miao.work2010@gmail.com>
2026-03-29 17:35:12 +0900

    remove CdpClient, uninstall ws (not needed yet)

--- src/page-runner.ts
+++ src/page-runner.ts
@@ -1,10 +1,8 @@
 import type { Runner } from './runner.js';
-import { CdpClient } from './cdp/client.js';
 import { openTab, closeTab } from './cdp/tab.js';
 
 export abstract class PageRunner<TParams, TResult> implements Runner<TParams, TResult> {
   protected params!: TParams;
-  protected cdp!: CdpClient;
   private tabId!: string;
 
   async run(params: TParams): Promise<TResult> {
@@ -21,14 +19,12 @@ export abstract class PageRunner<TParams, TResult> implements Runner<TParams, TR
   }
 
   async dispose(): Promise<void> {
-    this.cdp?.close();
     if (this.tabId) await closeTab(this.tabId).catch(() => {});
   }
 
   protected async openTab(url: string): Promise<void> {
     const tab = await openTab(url);
     this.tabId = tab.id;
-    this.cdp = await CdpClient.connect(tab.webSocketDebuggerUrl);
   }
 
   abstract navigate(): Promise<void>;
