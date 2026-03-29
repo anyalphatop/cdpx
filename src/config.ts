8799167df439eb5fb86fe259884ffcc47a6b578f
苗雨润 <miao.work2010@gmail.com>
2026-03-29 18:15:32 +0900

    Change default pollInterval to 100ms

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,6 @@ export const config = {
     host: process.env.CDPX_HOST ?? 'localhost',
     port: parseInt(process.env.CDPX_PORT ?? '9222', 10),
     timeout: parseInt(process.env.CDPX_TIMEOUT ?? '5000', 10),
-    pollInterval: parseInt(process.env.CDPX_POLL_INTERVAL ?? '500', 10),
+    pollInterval: parseInt(process.env.CDPX_POLL_INTERVAL ?? '100', 10),
   },
 };
