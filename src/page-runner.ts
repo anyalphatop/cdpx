92845bb909d49d7bb5a0a5523f6cc7ac3bffcbc8
苗雨润 <miao.work2010@gmail.com>
2026-03-29 17:30:07 +0900

    merge CdpPageRunner into PageRunner, remove intermediate class

--- src/page-runner.ts
+++ src/page-runner.ts
@@ -1,15 +1,30 @@
 import type { Runner } from './runner.js';
+import { CdpClient } from './cdp/client.js';
+import { openTab, closeTab } from './cdp/tab.js';
 
 export abstract class PageRunner<TParams, TResult> implements Runner<TParams, TResult> {
   protected params!: TParams;
+  protected cdp!: CdpClient;
+  private tabId!: string;
 
   async run(params: TParams): Promise<TResult> {
     this.params = params;
-    await this.navigate();
-    await this.ready();
-    await this.interact();
-    await this.settle();
-    return this.extract();
+    try {
+      await this.navigate();
+      await this.ready();
+      await this.interact();
+      await this.settle();
+      return await this.extract();
+    } finally {
+      this.cdp?.close();
+      if (this.tabId) await closeTab(this.tabId).catch(() => {});
+    }
+  }
+
+  protected async openTab(url: string): Promise<void> {
+    const tab = await openTab(url);
+    this.tabId = tab.id;
+    this.cdp = await CdpClient.connect(tab.webSocketDebuggerUrl);
   }
 
   abstract navigate(): Promise<void>;
--- src/runners/weibo/aisearch.ts
+++ src/runners/weibo/aisearch.ts
@@ -1,4 +1,4 @@
-import { CdpPageRunner } from '../../cdp-page-runner.js';
+import { PageRunner } from '../../page-runner.js';
 
 export interface AisearchParams {
   query: string;
@@ -8,7 +8,7 @@ export interface AisearchResult {
   query: string;
 }
 
-export class AisearchRunner extends CdpPageRunner<AisearchParams, AisearchResult> {
+export class AisearchRunner extends PageRunner<AisearchParams, AisearchResult> {
   async navigate(): Promise<void> {
     const url = `https://s.weibo.com/aisearch?q=${encodeURIComponent(this.params.query)}&Refer=aisearch_aisearch`;
     await this.openTab(url);
