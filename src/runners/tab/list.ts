aa4b7c22b2f10a3184f79f64280ae0fe0c31cb77
苗雨润 <miao.work2010@gmail.com>
2026-04-13 11:09:15 +0900

    remove redundant comment in TabListRunner

--- src/runners/tab/list.ts
+++ src/runners/tab/list.ts
@@ -9,7 +9,6 @@ export interface Tab {
 export class TabListRunner implements Runner<void, Tab[]> {
   async run(): Promise<Tab[]> {
     const context = await getContext();
-    // 收集当前 context 下所有标签页信息
     return Promise.all(
       context.pages().map(async (page) => ({
         title: await page.title(),
